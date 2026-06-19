b1f83c0d89874b641fbe94586773904487803247
mxmkiv <maxim.kiv@gmail.com>
2026-06-19 23:27:59 +0300

    feat: implement repository layer
    - add CRUDL and period sum operations
    - add Repository interface

--- internal/domain/subscription.go
+++ internal/domain/subscription.go
@@ -7,9 +7,26 @@ import (
 )
 
 type Subscription struct {
+	ID          uuid.UUID
 	ServiceName string
 	Price       int
 	UserID      uuid.UUID
 	StartDate   time.Time
-	EndDate     time.Time
+	EndDate     *time.Time
+}
+
+type CreateDTO struct {
+	ServiceName string
+	Price       int
+	UserID      uuid.UUID
+	StartDate   time.Time
+	EndDate     *time.Time
+}
+
+type UpdateDTO struct {
+	ServiceName string
+	Price       int
+	UserID      uuid.UUID
+	StartDate   time.Time
+	EndDate     *time.Time
 }
